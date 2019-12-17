d73381721c1e460417f008f46279294ee7b7c1cc
OssFate <mantis1512@gmail.com>
2019-12-16 19:14:15 -0500

    Day one

--- src/day/mod.rs
+++ src/day/mod.rs
@@ -20,9 +20,41 @@
 ///////
 
 pub mod one {
-    fn first_problem(file: &String) {}
+    fn first_problem(file: &String) {
+        let mut sum: i32 = 0;
+        for line in file.lines() {
+            let parsed = line.parse::<i32>().unwrap();
+            let gas_needed = parsed / 3;
+            let gas_needed = gas_needed - 2;
 
-    fn second_problem(file: &String) {}
+            sum += gas_needed;
+        }
+
+        println!("Ammount of fuel needed is: {}", sum);
+    }
+
+    fn second_problem(file: &String) {
+        let mut total_fuel: i32 = 0;
+        for line in file.lines() {
+            let parsed = line.parse::<i32>().unwrap();
+            let gas_needed = parsed / 3;
+            let gas_needed = gas_needed - 2;
+
+            total_fuel += gas_needed;
+
+            let mut temp = gas_needed;
+            while temp > 0 {
+                // println!("1: {}", temp);
+                temp = temp / 3;
+                temp = temp - 2;
+                if temp > 0 {
+                    total_fuel += temp;
+                }
+            }
+        }
+
+        println!("Ammount of total fuel needed is: {}", total_fuel);
+    }
 
     pub fn do_work(input: Option<String>) {
         let contents = match input {
